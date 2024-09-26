7f555d2b467d551a14698030a17571be542de853
Held Grijo <held.grijo@gmail.com>
2024-09-26 16:24:51 -0400

    fix: corrige problema com índices de uma lista de componentes

--- components/CardGallery.tsx
+++ components/CardGallery.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Card } from "flowbite-react";
 
 type CardData = {
@@ -11,18 +12,18 @@ type CardGalleryProps = {
   cards: CardData[];
 };
 
-export default function CardGallery({ cards }: CardGalleryProps) {
+export default function CardGallery({ cards }: Readonly<CardGalleryProps>) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-      {cards.map((card, index) => (
-        <a href={card.link} key={index} target="_blank" rel="noopener noreferrer">
+      {cards.map((card) => (
+        <Link href={card.link} key={card.title}  rel="noopener noreferrer">
           <Card imgSrc={card.image}>
             <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
               {card.title}
             </h5>
             <p className="text-gray-700 dark:text-gray-400">{card.description}</p>
           </Card>
-        </a>
+        </Link>
       ))}
     </div>
   );
